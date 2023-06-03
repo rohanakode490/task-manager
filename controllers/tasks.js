d18e7fa69a4eea6a6e6e618643aa2d6901fae955
Rohan <rohanakode52@gmail.com>
2023-06-03 23:47:44 +0530

    Routes testing commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,11 +3,11 @@ const getAllTasks = (req, res) => {
 };
 
 const createTask = (req, res) => {
-  res.send("create Task");
+  res.json(req.body);
 };
 
 const getTask = (req, res) => {
-  res.send("get one Task");
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
