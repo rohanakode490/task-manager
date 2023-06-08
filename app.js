a957977575fe356a56435c9e6416f92486cceed0
Rohan <rohanakode52@gmail.com>
2023-06-09 00:48:35 +0530

    404 middleware

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-require('dotenv').config()
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/", (req, res) => {
-  res.send("Task Manager");
-});
-
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,14 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
+
     res.status(200).json({ tasks });
+    // res.status(200).json({ tasks, amount: tasks.length });
+    // res
+    //   .status(200)
+    //   .json({ status: "success", data: { tasks, nbHits: tasks.length } });
+
+
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -38,11 +45,11 @@ const getTask = async (req, res) => {
 const updateTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
-    
+
     const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
       new: true,
-      runValidators: true
-    });  //{{condition}, {data}, options}
+      runValidators: true,
+    }); //{{condition}, {data}, options}
 
     if (!task) {
       return res.status(404).json({ msg: `No task with id: ${taskID}` });
